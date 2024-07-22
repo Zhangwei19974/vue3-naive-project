3d03c428993ea3859cb70f3620bcd5f160174312
zhangwei <m18860899637@163.com>
2024-07-22 14:59:02 +0800

    添加路经选择测试

--- src/views/MainView/DataDashView/index.vue
+++ src/views/MainView/DataDashView/index.vue
@@ -3,11 +3,64 @@
     asdasd
 
     <n-button @click="getFiles">点击</n-button>
+    <n-cascader
+      :options="cascaderOptions"
+      value-field="name"
+      label-field="name"
+      check-strategy="child"
+    ></n-cascader>
   </div>
 </template>
 
 <script setup lang="ts">
+import { set } from 'lodash-es';
+import { CascaderOption, NCascader } from 'naive-ui';
+
 const route = useRoute();
+const cascaderOptions = computed(() => {
+  let files = import.meta.glob('@/**/**.vue');
+
+  let options = getCascaderOptions(Object.keys(files));
+  console.log(options);
+
+  return options;
+});
+
+function getCascaderOptions(paths: string[]): CascaderOption[] {
+  let tree: CascaderOption[] = [];
+  paths.forEach((path) => {
+    const parts = path.split('/').filter(Boolean);
+    let currentLevel = tree;
+    let currentNode = null as any;
+
+    parts.forEach((part, index) => {
+      const isLastPart = index === parts.length - 1;
+      const existingNode = currentLevel.find((node) => node.name === part);
+
+      if (existingNode) {
+        currentNode = existingNode;
+      } else {
+        if (!isLastPart) {
+          currentNode = {
+            name: part,
+            children: [],
+          };
+        } else {
+          currentNode = {
+            name: part,
+          };
+        }
+        currentLevel.push(currentNode);
+      }
+
+      if (!isLastPart) {
+        currentLevel = currentNode.children;
+      }
+    });
+  });
+
+  return tree;
+}
 onMounted(() => {
   console.log(route.params, 'asdasdasd');
 });
